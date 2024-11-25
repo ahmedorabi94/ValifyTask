a7caacae2fcaf3baa9dd4f40ccd76a2a0eae7928
Ahmed Orabi <eng.ahmedorabi94@gmail.com>
2024-11-25 22:22:23 +0200

    add validation to register screen

--- app/src/main/java/com/example/valifytask/presentation/RegisterViewModel.kt
+++ app/src/main/java/com/example/valifytask/presentation/RegisterViewModel.kt
@@ -7,6 +7,11 @@ import com.example.valifytask.db.UserInfo
 import com.example.valifytask.use_cases.AddUserInfoUseCase
 import com.example.valifytask.use_cases.GetLastUserInfoUseCase
 import com.example.valifytask.use_cases.UpdateUserInfoUseCase
+import com.example.valifytask.validation.RegisterScreenState
+import com.example.valifytask.validation.ValidateEmail
+import com.example.valifytask.validation.ValidatePassword
+import com.example.valifytask.validation.ValidatePhone
+import com.example.valifytask.validation.ValidateUserName
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import timber.log.Timber
@@ -17,16 +22,51 @@ import javax.inject.Inject
 class RegisterViewModel @Inject constructor(
     private val addUserInfoUseCase: AddUserInfoUseCase,
     private val updateUserInfoUseCase: UpdateUserInfoUseCase,
-    private val getLastUserInfoUseCase: GetLastUserInfoUseCase
+    private val getLastUserInfoUseCase: GetLastUserInfoUseCase,
 ): ViewModel(){
 
 
-    var useInfo = UserInfo(userName = "", email = "",  phoneNumber = "")
+    var state = RegisterScreenState()
 
+   // var useInfo = UserInfo(userName = "", email = "",  phoneNumber = "")
+    //val hasError = MutableStateFlow<Boolean>(false)
+
+
+    private val validatePassword = ValidatePassword()
+    private val validateUserName: ValidateUserName = ValidateUserName()
+    private val validateEmail  = ValidateEmail()
+    private val validatePhone = ValidatePhone()
+
+
+    fun saveData() : Boolean{
+
+        val usernameResult = validateUserName.execute(state.userName)
+        val emailResult = validateEmail.execute(state.email)
+        val passwordResult = validatePassword.execute(state.password)
+        val phoneResult = validatePhone.execute(state.phoneNumber)
+
+
+        state = state.copy(
+            userNameError = usernameResult.errorMessage,
+            emailError =  emailResult.errorMessage,
+            passwordError = passwordResult.errorMessage,
+            phoneNumberError = phoneResult.errorMessage,
+        )
+
+        return listOf(
+            usernameResult,
+            emailResult,
+            phoneResult,
+            passwordResult
+        ).any { !it.successful }
+
+
+    }
 
     fun addUserInfo(){
         viewModelScope.launch {
-            addUserInfoUseCase.execute(useInfo)
+            val userInfo = UserInfo(userName = state.userName, email = state.email , phoneNumber = state.phoneNumber)
+            addUserInfoUseCase.execute(userInfo)
         }
 
     }
--- app/src/main/java/com/example/valifytask/presentation/RegistrationScreen.kt
+++ app/src/main/java/com/example/valifytask/presentation/RegistrationScreen.kt
@@ -1,43 +1,31 @@
 package com.example.valifytask.presentation
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardCapitalization
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import com.example.valifytask.R
 import com.example.valifytask.navigation.NavigationItem
 
 
@@ -50,10 +38,18 @@ fun RegistrationScreen(
 ) {
 
     val localFocusManager = LocalFocusManager.current
+
+
     val usernameMutable = remember { mutableStateOf("") }
     val phoneNumberMutable = remember { mutableStateOf("") }
     val emailMutable = remember { mutableStateOf("") }
     val passwordMutable = remember { mutableStateOf("") }
+
+    val usernameErrorMutable = remember { mutableStateOf("") }
+    val emailErrorMutable = remember { mutableStateOf("") }
+    val phoneErrorMutable = remember { mutableStateOf("") }
+    val passwordErrorMutable = remember { mutableStateOf("") }
+
 
     Column(
         modifier = Modifier
@@ -63,201 +59,71 @@ fun RegistrationScreen(
 
 
         Spacer(Modifier.height(20.dp))
-        TextField(
-            value = usernameMutable.value,
-            onValueChange = {
-                usernameMutable.value = it
-                registerViewModel.useInfo = registerViewModel.useInfo.copy(userName = it)
+        CustomTextField(
+            usernameMutable,
+            usernameErrorMutable,
+            KeyboardActions(onNext = { localFocusManager.moveFocus(FocusDirection.Down) }),
+            "Username",
+            onChanged = {
+                registerViewModel.state = registerViewModel.state.copy(userName = it)
+            }
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        CustomTextField(
+            phoneNumberMutable,
+            phoneErrorMutable,
+            KeyboardActions(onNext = { localFocusManager.moveFocus(FocusDirection.Down) }),
+            "Phone Number",
+            onChanged = {
+                registerViewModel.state = registerViewModel.state.copy(phoneNumber = it)
+            }
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        CustomTextField(
+            emailMutable,
+            emailErrorMutable,
+            KeyboardActions(onNext = { localFocusManager.moveFocus(FocusDirection.Down) }),
+            "Email",
+            onChanged = {
+                registerViewModel.state = registerViewModel.state.copy(email = it)
+            }
 
-            },
-            trailingIcon = {
-
-            },
-            placeholder = {
-                Text(
-                    text = "Username",
-                    color = colorResource(id = R.color.white),
-                    fontSize = 17.sp,
-                    fontFamily = FontFamily.SansSerif
-                )
-            },
-            keyboardActions = KeyboardActions(onNext = {
-                localFocusManager.moveFocus(FocusDirection.Down)
-            }),
-            keyboardOptions = KeyboardOptions(
-                capitalization = KeyboardCapitalization.None,
-                autoCorrect = true,
-                keyboardType = KeyboardType.Text,
-                imeAction = ImeAction.Next
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 12.dp, end = 12.dp)
-                .defaultMinSize(minHeight = 44.dp)
-                .border(
-                    width = 1.dp,
-                    color = colorResource(id = R.color.white),
-                    shape = RoundedCornerShape(6.dp)
-                )
-                .scale(scaleY = 0.9F, scaleX = 1F),
-            textStyle = TextStyle(
-                color = colorResource(id = R.color.black),
-                fontSize = 17.sp,
-                fontFamily = FontFamily.SansSerif
-            ),
-            shape = RoundedCornerShape(6.dp),
-            singleLine = true,
-
-            )
-
+        )
         Spacer(Modifier.height(8.dp))
-        TextField(
-            value = phoneNumberMutable.value,
-            onValueChange = {
-                phoneNumberMutable.value = it
-                registerViewModel.useInfo = registerViewModel.useInfo.copy(phoneNumber = it)
-            },
-            trailingIcon = {
-
-            },
-            placeholder = {
-                Text(
-                    text = "Phone Number",
-                    color = colorResource(id = R.color.white),
-                    fontSize = 17.sp,
-                    fontFamily = FontFamily.SansSerif
-                )
-            },
-            keyboardActions = KeyboardActions(onNext = {
-                localFocusManager.moveFocus(FocusDirection.Down)
-            }),
-            keyboardOptions = KeyboardOptions(
-                capitalization = KeyboardCapitalization.None,
-                autoCorrect = true,
-                keyboardType = KeyboardType.Phone,
-                imeAction = ImeAction.Next
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 12.dp, end = 12.dp)
-                .defaultMinSize(minHeight = 44.dp)
-                .border(
-                    width = 1.dp,
-                    color = colorResource(id = R.color.white),
-                    shape = RoundedCornerShape(6.dp)
-                )
-                .scale(scaleY = 0.9F, scaleX = 1F),
-            textStyle = TextStyle(
-                color = colorResource(id = R.color.black),
-                fontSize = 17.sp,
-                fontFamily = FontFamily.SansSerif
-            ),
-            shape = RoundedCornerShape(6.dp),
-            singleLine = true,
-
-            )
-
-        Spacer(Modifier.height(8.dp))
-        TextField(
-            value = emailMutable.value,
-            onValueChange = {
-                emailMutable.value = it
-                registerViewModel.useInfo = registerViewModel.useInfo.copy(email = it)
-
-            },
-            trailingIcon = {
-
-            },
-            placeholder = {
-                Text(
-                    text = "Email",
-                    color = colorResource(id = R.color.white),
-                    fontSize = 17.sp,
-                    fontFamily = FontFamily.SansSerif
-                )
-            },
-            keyboardActions = KeyboardActions(onNext = {
-                localFocusManager.moveFocus(FocusDirection.Down)
-            }),
-            keyboardOptions = KeyboardOptions(
-                capitalization = KeyboardCapitalization.None,
-                autoCorrect = true,
-                keyboardType = KeyboardType.Email,
-                imeAction = ImeAction.Next
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 12.dp, end = 12.dp)
-                .defaultMinSize(minHeight = 44.dp)
-                .border(
-                    width = 1.dp,
-                    color = colorResource(id = R.color.white),
-                    shape = RoundedCornerShape(6.dp)
-                )
-                .scale(scaleY = 0.9F, scaleX = 1F),
-            textStyle = TextStyle(
-                color = colorResource(id = R.color.black),
-                fontSize = 17.sp,
-                fontFamily = FontFamily.SansSerif
-            ),
-            shape = RoundedCornerShape(6.dp),
-            singleLine = true,
-
-            )
-        Spacer(Modifier.height(8.dp))
-        TextField(
-            value = passwordMutable.value,
-            onValueChange = {
-                passwordMutable.value = it
-
-            },
-            trailingIcon = {
-
-            },
-            placeholder = {
-                Text(
-                    text = "Password",
-                    color = colorResource(id = R.color.white),
-                    fontSize = 17.sp,
-                    fontFamily = FontFamily.SansSerif
-                )
-            },
-            keyboardActions = KeyboardActions(onNext = {
-                localFocusManager.moveFocus(FocusDirection.Down)
-            }),
-            keyboardOptions = KeyboardOptions(
-                capitalization = KeyboardCapitalization.None,
-                autoCorrect = true,
-                keyboardType = KeyboardType.Password,
-                imeAction = ImeAction.Next
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 12.dp, end = 12.dp)
-                .defaultMinSize(minHeight = 44.dp)
-                .border(
-                    width = 1.dp,
-                    color = colorResource(id = R.color.white),
-                    shape = RoundedCornerShape(6.dp)
-                )
-                .scale(scaleY = 0.9F, scaleX = 1F),
-            textStyle = TextStyle(
-                color = colorResource(id = R.color.black),
-                fontSize = 17.sp,
-                fontFamily = FontFamily.SansSerif
-            ),
-            shape = RoundedCornerShape(6.dp),
-            singleLine = true,
-
-            )
-        Spacer(Modifier.height(8.dp))
-
+        CustomTextField(
+            passwordMutable,
+            passwordErrorMutable,
+            KeyboardActions(onNext = { localFocusManager.moveFocus(FocusDirection.Down) }),
+            "Password",
+            onChanged = {
+                registerViewModel.state = registerViewModel.state.copy(password = it)
+            }
+        )
 
         Button(
             onClick = {
 
-                registerViewModel.addUserInfo()
-                navController?.navigate(NavigationItem.CaptureImage.route)
+
+                if (registerViewModel.saveData()) {
+                    if (registerViewModel.state.userNameError != null)
+                        usernameErrorMutable.value = registerViewModel.state.userNameError ?: ""
+                    else usernameErrorMutable.value = ""
+                    if (registerViewModel.state.phoneNumberError != null)
+                        phoneErrorMutable.value = registerViewModel.state.phoneNumberError ?: ""
+                    else phoneErrorMutable.value = ""
+
+                    if (registerViewModel.state.emailError != null)
+                        emailErrorMutable.value = registerViewModel.state.emailError ?: ""
+                    else emailErrorMutable.value = ""
+
+                    if (registerViewModel.state.passwordError != null)
+                        passwordErrorMutable.value = registerViewModel.state.passwordError ?: ""
+                    else passwordErrorMutable.value = ""
+                } else {
+                    registerViewModel.addUserInfo()
+                    navController?.navigate(NavigationItem.CaptureImage.route)
+                }
+
 
             },
             shape = RoundedCornerShape(14.dp),
